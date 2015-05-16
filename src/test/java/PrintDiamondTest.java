18dfe1d900f7691ce8882209aeee362a0b4a0b65
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-05-16 21:55:55 +0200

    calculate level character

--- src/test/java/PrintDiamondTest.java
+++ src/test/java/PrintDiamondTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 
 /**
  * Class to test the print diamond katal.
- *
+ * <p/>
  * [x]ToTest: for input A print diamond
  * [X]ToTest: for input B print diamond
  * [X]ToTest: for input C print diamond
@@ -34,42 +34,40 @@ public class PrintDiamondTest {
     @Test
     public void printDiamond_forC() {
         final String output[] = printDiamond('C');
-        assertThat(output[0], is( "  A  "));
-        assertThat(output[1], is( " B B "));
-        assertThat(output[2], is( "C   C"));
-        assertThat(output[3], is( " B B "));
-        assertThat(output[4], is( "  A  "));
+        assertThat(output[0], is("  A  "));
+        assertThat(output[1], is(" B B "));
+        assertThat(output[2], is("C   C"));
+        assertThat(output[3], is(" B B "));
+        assertThat(output[4], is("  A  "));
     }
 
     private String[] printDiamond(final char character) {
         if (character == 'C') {
-
             return new String[]{
-                    format('A', 5, 0)
-                    , format('B', 5, 1)
-                    , format('C', 5, 2)
-                    , format('B', 5, 1)
-                     , format('A', 5, 0)
+                    format(5, 0)
+                    , format(5, 1)
+                    , format(5, 2)
+                    , format(5, 1)
+                    , format(5, 0)
             };
         } else if (character == 'B') {
-
             return new String[]{
-                    format('A', 3, 0)
-                    , format('B', 3, 1)
-                    , format('A', 3, 0)
+                    format(3, 0)
+                    , format(3, 1)
+                    , format(3, 0)
             };
         } else {
 
-            return new String[]{format('A', 1, 0)};
+            return new String[]{format(1, 0)};
         }
 
     }
 
-    private String format(final char character, final int width, final int level) {
+    private String format(final int width, final int level) {
         char[] chars = new char[width];
         Arrays.fill(chars, ' ');
-        chars[(width/2) - level] = character;
-        chars[(width/2) + level] = character;
+        chars[(width/2) - level] = (char) ('A' + level);
+        chars[(width/2) + level] = (char) ('A' + level);
         return String.valueOf(chars);
     }
 
