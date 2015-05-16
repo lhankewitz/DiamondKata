2997863b6ae16f2c96cd74714410efca08e27796
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-05-16 10:01:44 +0200

    added test env

---
+++ src/test/java/PrintDiamondTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class to ...
+ *
+ * @author lumiha
+ * @since 16/05/15.
+ */
+public class PrintDiamondTest {
+
+    @Test
+    public void env_ok() {
+        assertThat("ok", is("ok"));
+    }
+}
