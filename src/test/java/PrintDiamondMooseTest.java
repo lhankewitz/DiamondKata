9961541d833fc83738cc7acbf2008038d3afe2ac
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-05-18 08:48:30 +0200

    started test the moose way (evolving test)

---
+++ src/test/java/PrintDiamondMooseTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class to test printing the diamond the moose way:
+ *
+ * <a href="http://claysnow.co.uk/recycling-tests-in-tdd/">Meaning of moose</a>
+ *
+ * @author lumiha
+ * @since 18/05/15.
+ */
+public class PrintDiamondMooseTest {
+
+    @Test
+    public void printDiamond_forA() {
+        final String result = printDiamond("A");
+        assertThat(result, is("A"));
+    }
+
+    private String printDiamond(final String character) {
+        return "A";
+    }
+}
